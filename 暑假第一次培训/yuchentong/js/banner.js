8b550ea027041d430f06549bde40a371f857719b
yuchentong <example.com>
2021-07-14 17:43:34 +0800

    add

---
+++ 暑假第一次培训/yuchentong/js/banner.js
@@ -0,0 +1,11 @@
+
+$(function() {
+    jQuery('.fullwidthbanner').revolution({
+        delay: 9000,
+        startwidth: 1170,
+        startheight:450,
+        hideThumbs: 10,
+        fullWidth: "on",
+        forceFullWidth: "on"
+    });
+});
