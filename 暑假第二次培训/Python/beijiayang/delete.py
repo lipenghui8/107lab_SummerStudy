4b91193e037174b5a43967ae593e42b38a9f7e9a
‘fujingqi’ <1070673506@qq.com>
2021-07-25 15:08:34 +0800

    bjy

---
+++ 暑假第二次培训/Python/beijiayang/delete.py
@@ -0,0 +1,36 @@
+#1.导包
+
+import pymysql
+#链接数据库
+connc = pymysql.Connect(
+        host='192.168.3.20',
+        user='root',
+        password="0159",
+        database='orders',
+        port=3306,
+        charset='utf8'
+)
+
+#3创建游标对象
+cur = connc.cursor()
+
+
+try:
+        #4编写 删除表的数据sql
+        sql='delete from beijiayang where id=1;'
+
+        #5.使用游标对象执行sql
+        cur.execute(sql,body)
+
+        #6 .提交操作
+        connc.commit()
+
+        #try的优化
+except Exception as  e:
+        print('操作失败：',e)
+finally:
+        cur.close()
+
+        connc.close()
+
+print('game over')
